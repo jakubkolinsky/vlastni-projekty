0ee870569c6a1d6d56b657ea87936224844215e5
jakubkolinsky <kolinsky1995@gmail.com>
2024-06-22 18:53:37 +0200

    ukolnicek postylovani

--- ukolnicek/ukolnicek.php
+++ ukolnicek/ukolnicek.php
@@ -55,42 +55,77 @@ if (array_key_exists("zrusit-upravu-submit", $_POST)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Úkolníček</title>
+    <style>
+        body {
+            justify-content: center;
+        }
+
+        .container {
+            text-align: center;
+        }
+
+        h1 {
+            text-align: center;
+        }
+
+        .seznam .center {
+            align-items: center;
+            text-align: center;
+            margin-left: auto;
+            margin-right: auto;
+            width: 50%;
+        }
+
+        #xy {
+            font-weight: bold;
+        }
+
+        .container input[type="text"],
+        .container textarea {
+            width: 25%;
+        }
+
+        img {
+            width: 13px;
+            height: auto;
+        }
+
+    </style>
 </head>
 
 <body>
     <h1>Úkolníček</h1>
+    <div class="container">
+        <form action="" method="post">
+            <input type="text" name="nazev-poznamky" placeholder="Název" width="150px">
+            <br>
+            <textarea name="text-poznamky" rows='5'></textarea>
+            <br>
+            <button type="submit" name="vytvoreni-submit">Vytvořit</button>
+        </form>
+    </div>
+    <div class="seznam">
+        <table class="center">
+            <?php
+            if (array_key_exists("uprava", $_GET)) {
+                echo "<tr> <form method='post'>
+                <td> <input type='text' name='uprava-nazev' value='$nazevPoznamky'> </td>
+                <td> <textarea name='uprava-text'>$textPoznamky</textarea></td>
+                <td> <button type='submit' name='uprava-submit'><img src='./img/pencil.png'></button></td>
+                <td> <button type='submit' name='zrusit-upravu-submit'><img src='./img/close.png'></button></td></form>";
+            };
+            foreach ($polePoznamek as $klic) {
+                echo "<tr>
+                <td id='xy'> $klic[nazev]</td>
+                <td> $klic[poznamka]</td>
+                <td><a href='?smazat={$klic['id']}'><img src='./img/trash_bin.png'></a></td>
+                <td><a href='?uprava={$klic['id']}'><img src='./img/pencil.png'></a></td>
+                </tr>";
+            };
 
-    <form action="" method="post">
-        <label for="xy">Název</label>
-        <input type="text" name="nazev-poznamky" id="xy">
-        <textarea name="text-poznamky" id=""></textarea>
-        <button type="submit" name="vytvoreni-submit">Vytvořit</button>
-    </form>
-    <table border="1px">
-        <th>Nazev</th>
-        <th>Text poznámky</th>
-        <th>Smazání</th>
-        <th>Úprava</th>
-        <?php
-if (array_key_exists("uprava", $_GET)) {
-            echo "<tr> <form method='post'>
-            <td> <input type='text' name='uprava-nazev' id='xy' value='$nazevPoznamky'> </td>
-            <td> <textarea name='uprava-text' id=>$textPoznamky</textarea></td>
-            <td> <button type='submit' name='uprava-submit'>Upravit</button></td>
-            <td> <button type='submit' name='zrusit-upravu-submit'>Zrušit</button></td></form>";
-        };
-        foreach ($polePoznamek as $klic) {
-
-            echo "<tr>
-            <td> $klic[nazev]</td>
-            <td> $klic[poznamka]</td>
-            <td><a href='?smazat={$klic['id']}'>Smazani</a></td>
-            <td><a href='?uprava={$klic['id']}'>Úprava</a></td>
-            </tr>";
-        };
-        
-        ?>
-    </table>
+            ?>
+        </table>
+    </div>
     <!-- Slavnostně přísahám, že k vytvoření Úkolníčku nebyla použita umělá inteligence -->
 </body>
 
